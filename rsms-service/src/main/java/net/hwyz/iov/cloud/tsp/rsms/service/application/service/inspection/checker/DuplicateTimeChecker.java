390ff5dc5a43be303e94c003b7964ba633891d7b
hwyz_leo <hwyz.leo.ye@gmail.com>
2025-08-29 10:22:57 +0800

    改进: 优化国标检测报告处理策略

--- rsms-service/src/main/java/net/hwyz/iov/cloud/tsp/rsms/service/application/service/inspection/checker/DuplicateTimeChecker.java
+++ rsms-service/src/main/java/net/hwyz/iov/cloud/tsp/rsms/service/application/service/inspection/checker/DuplicateTimeChecker.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import net.hwyz.iov.cloud.tsp.rsms.service.application.service.inspection.AbstractChecker;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -27,7 +28,7 @@ public class DuplicateTimeChecker extends AbstractChecker {
         this.item = item;
         this.count = 0L;
         this.errorCount = 0L;
-        this.timestamps = Set.of();
+        this.timestamps = new HashSet<>();
     }
 
     /**
