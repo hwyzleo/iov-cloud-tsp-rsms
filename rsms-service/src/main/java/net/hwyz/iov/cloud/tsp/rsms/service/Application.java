f533cfd08e45832e2e5cd48c061458fa65b79ae2
hwyz_leo <hwyz.leo.ye@gmail.com>
2025-08-29 13:38:05 +0800

    配置: 启用异步

--- rsms-service/src/main/java/net/hwyz/iov/cloud/tsp/rsms/service/Application.java
+++ rsms-service/src/main/java/net/hwyz/iov/cloud/tsp/rsms/service/Application.java
@@ -6,6 +6,7 @@ import net.hwyz.iov.cloud.framework.security.annotation.EnableCustomFeignClients
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * 启动类
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author hwyz_leo
  */
 @Slf4j
+@EnableAsync
 @EnableDiscoveryClient
 @EnableCustomFeignClients
 @SpringBootApplication
